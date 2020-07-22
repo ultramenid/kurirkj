3d32751ec5c65c4c18107516970acbc341768236
ultramenid <malichamdan@gmail.com>
2020-07-22 13:59:44 +0700

    change qrcode & deny acces assets folder

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -2,11 +2,11 @@
     <div class="row justify-content-center">
         <div class="col-lg-6 my-4 ">
                     <div class="row justify-content-center">
-                    <img id="btn-scan-qr" class="text-center" src="https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=https://www.youtube.com/c/koranjakartadigital&choe=UTF-8" title="Link Youtube KJ" />
-                    <h5 id="idklik">Klik Qrcode untuk mengaktifkan kamera</h5>
-                    
-        
-            <canvas hidden="" id="qr-canvas"></canvas>
+                    <img id="btn-scan-qr" class="text-center" src="<?=base_url("assets/img/perusahaan/qrcode-logo.png") ?>" style="height:300px; width: 300px;" title="Link Youtube KJ" />
+                    <div class="col-lg-8">
+                        <h6 class="text-center" id="idklik">Klik Qrcode untuk mengaktifkan kamera</h6>
+                    </div>
+                    <canvas hidden="" id="qr-canvas"></canvas>
                </div>
                     <div id="qr-result" hidden="">
             </div>
